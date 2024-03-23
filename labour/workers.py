cf658e61af7c8ecaf343dc2cf53ac3abc8b6521d
NikhilTirunagiri <nikhiltirunagiri@outlook.com>
2024-03-23 19:08:33 -0400

    .env setup

--- labour/workers.py
+++ labour/workers.py
@@ -1,4 +1,15 @@
 import os
 import requests
-import openai
+import openai # pip install openai
 
+from dotenv import load_dotenv # pip install python-dotenv
+load_dotenv()
+
+# -- bing web search api test1 --
+# python -m pip install azure-cognitiveservices-search-customsearch
+
+from azure.cognitiveservices.search.customsearch import CustomSearchClient
+from msrest.authentication import CognitiveServicesCredentials
+
+subscription_key = os.getenv('OPENAI_API_KEY')
+endpoint = os.getenv('OPENAI_API_ENDPOINT')
